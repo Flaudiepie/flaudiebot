25faf22f7eb86ac5997448fc3665d545da6f2176
Florian Böse <florian.wagner01@Hotmail.com>
2023-05-01 16:56:59 +0200

    Added Docker File

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.20"
-
+    id("io.ktor.plugin") version "2.3.0"
+    application
 }
 
 group = "org.example"
@@ -8,6 +9,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application{
+    mainClass.set("de.boese.soundbot.SoundbotApplication")
 }
 
 dependencies {
@@ -21,4 +26,19 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+ktor {
+    docker {
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        localImageName.set("soundbot")
+        imageTag.set("0.0.1-preview")
+        externalRegistry.set(
+            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+                appName = provider { "flaudiebot" },
+                username = providers.environmentVariable("DOCKER_HUB_PERSONAL_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PERSONAL_PASSWORD")
+            )
+        )
+    }
 }
